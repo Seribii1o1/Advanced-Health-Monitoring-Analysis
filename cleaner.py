39d77d5f435ad514ccc5c1a08c5a3dd5e033433a
Seribii1o1 <164199835+Seribii1o1@users.noreply.github.com>
2024-12-02 21:20:48 -0500

    Commit 1

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,13 @@
 def filter_nondigits(data: list) -> list:
+    filtered_data = []
+    for item in data:
+        # Remove newline characters
+        item = item.strip('\n')
+
+        # Check if the string consists only of digits
+        if item.isdigit():
+            filtered_data.append(int(item))
+    return filtered_data
     """
     Filter all strings from list that are not integers
 
@@ -9,7 +18,6 @@ def filter_nondigits(data: list) -> list:
         list[int]: list of integers, with all non-digit strings removed
     """
     pass
-
 
 def filter_outliers(data: list) -> list:
     pass
