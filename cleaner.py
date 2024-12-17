030e56a44d3d0d17dd97ed9f86f18b67e717b7a2
Seribii1o1 <164199835+Seribii1o1@users.noreply.github.com>
2024-12-16 19:25:08 -0500

    commit 12

--- cleaner.py
+++ cleaner.py
@@ -1,13 +1,4 @@
 def filter_nondigits(data: list) -> list:
-    filtered_data = []
-    for item in data:
-        # Remove newline characters
-        item = item.strip('\n')
-
-        # Check if the string consists only of digits
-        if item.isdigit():
-            filtered_data.append(int(item))
-    return filtered_data
     """
     Filter all strings from list that are not integers
 
@@ -17,18 +8,27 @@ def filter_nondigits(data: list) -> list:
     Returns:
         list[int]: list of integers, with all non-digit strings removed
     """
+    filtered_data = []
+    for item in data:
+        # Remove newline characters
+        item = item.strip('\n')
+
+        # Check if the string consists only of digits
+        if item.isdigit():
+            filtered_data.append(int(item))
+    return filtered_data
 
 def filter_outliers(data: list) -> list:
+    """Filters out heart rate samples that are less than 30 or greater than 250.
+
+    Args:
+        data: A list of integer heart rate samples.
+
+    Returns:
+    A new list containing only the filtered heart rate samples.
+    """
     filtered_data = []
     for heart_rate in data:
       if 30 < heart_rate < 250:
          filtered_data.append(heart_rate)
     return filtered_data
-"""Filters out heart rate samples that are less than 30 or greater than 250.
-
-  Args:
-    data: A list of integer heart rate samples.
-
-  Returns:
-    A new list containing only the filtered heart rate samples.
-  """
