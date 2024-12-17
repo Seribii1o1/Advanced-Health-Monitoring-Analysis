1456ce8853eda1502626e2487b1d31394ef1f5ea
Seribii1o1 <164199835+Seribii1o1@users.noreply.github.com>
2024-12-16 20:13:43 -0500

    commit 16

--- metrics.py
+++ metrics.py
@@ -1,25 +1,46 @@
 import statistics
 def window_max(data: list, n: int) -> list:
-    maximums = []
-    for i in range(0, len(data), n):  # Increment by n for non-overlapping windows
-        maximums.append(round(max(data[i:i + n]), 2))
-    return maximums
-"""
+    """
     Calculate maximum value of every "n"-size window
 
     Args:
         data (list[int]): list of integers representing heart rate samples
         n (int): The size of your window
+
     Returns:
         list[int]: list of maximums from each window (size should be len(data)//6)
     """
+    maximums = []
+    for i in range(0, len(data), n):  # Increment by n for non-overlapping windows
+        maximums.append(round(max(data[i:i + n]), 2))
+    return maximums
+
 def window_average(data: list, n: int) -> list:
+    """Calculates the average of elements within sliding windows of size n.
+
+    Args:
+        data (list): The input list of numerical values.
+        n (int): The size of the sliding window.
+
+    Returns:
+        list: A list of averages, where each element is the average of a window of size n.
+    """
     averages = []
     for i in range(0, len(data), n):
         averages.append(round(statistics.mean(data[i:i + n]), 2))
     return averages
 
 def window_stddev(data: list, n: int) -> list:
+    """
+    Calculates the standard deviation of non-overlapping windows within a given list of data.
+
+    Args:
+        data (list): The input list of numerical data.
+        n (int): The size of each window.
+
+    Returns:
+        list: A list of standard deviations, one for each window.
+    """
     stddevs = []
     if not data or n <= 0: 
         return stddevs
